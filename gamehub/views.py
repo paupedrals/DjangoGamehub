fc9e0d13af0806468f7be7893a839ecea318a89b
Pau Pedrals <pau.pedrals@soltel.es>
2025-03-12 09:22:27 +0100

    test2

--- gamehub/views.py
+++ gamehub/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import redirect
 
 from django.shortcuts import render
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.htmlO")
 #This is a test
